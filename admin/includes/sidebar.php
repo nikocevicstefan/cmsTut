c22d60d4776073f4f14a147ba9d2e191bc886524
Stefan Nikocevic <nikocevicstefan@gmail.com>
2019-05-01 20:46:58 +0200

    Divided layout into components

--- admin/includes/sidebar.php
+++ admin/includes/sidebar.php
@@ -1,1 +1,40 @@
-<?php
+<div class="collapse navbar-collapse navbar-ex1-collapse">
+    <ul class="nav navbar-nav side-nav">
+        <li>
+            <a href="index.html"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
+        </li>
+        <li>
+            <a href="javascript:;" data-toggle="collapse" data-target="#posts_dropdown"><i class="fa fa-fw fa-arrows-v"></i>
+                Posts <i class="fa fa-fw fa-caret-down"></i></a>
+            <ul id="posts_dropdown" class="collapse">
+                <li>
+                    <a href="#">View All Posts</a>
+                </li>
+                <li>
+                    <a href="#">Add Posts</a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <a href="bootstrap-grid.html"><i class="fa fa-fw fa-wrench"></i> Categories</a>
+        </li>
+        <li class="active">
+            <a href="blank-page.html"><i class="fa fa-fw fa-file"></i> Comments</a>
+        </li>
+        <li>
+            <a href="javascript:;" data-toggle="collapse" data-target="#users"><i class="fa fa-fw fa-arrows-v"></i>
+                Users <i class="fa fa-fw fa-caret-down"></i></a>
+            <ul id="users" class="collapse">
+                <li>
+                    <a href="#">Dropdown Item</a>
+                </li>
+                <li>
+                    <a href="#">Dropdown Item</a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <a href="#"><i class="fa fa-fw fa-user"></i> Profile</a>
+        </li>
+    </ul>
+</div>
